315e8d32c2b4c5a4f489d3d28beeed3038a84956
吴禹衡 <wuyuheng@qudian.com>
2016-12-19 12:24:34 +0800

    初始化控制器

--- 唯美拍/AppDelegate.swift
+++ 唯美拍/AppDelegate.swift
@@ -12,10 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let mainVC :UIViewController = mainViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
         return true
     }
 
---
+++ 唯美拍/mainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  mainViewController.swift
+//  唯美拍
+//
+//  Created by WuYuHeng on 16/12/19.
+//  Copyright © 2016年 WuYuHeng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class mainViewController: UIViewController {
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+    }
+    
+
+}
